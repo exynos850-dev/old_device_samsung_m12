5b9408517bc73daaa20541424431c43c2d1ea1e9
Tim Zimmermann <tim@linux4.de>
2022-08-24 15:40:22 +0200

    m12: Inherit non_ab_device.mk
    * This is required to build applypatch for updating recovery
    
    Change-Id: I38053691f7724b5d6c30159c410d5d04ac136180

--- arrow_m12.mk
+++ arrow_m12.mk
@@ -7,6 +7,7 @@
 ## Inherit from generic products, most specific first
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 
 ## Inherit from m12 device
 $(call inherit-product, device/samsung/m12/device.mk)
