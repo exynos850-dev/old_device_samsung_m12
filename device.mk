156b35909fe904f511332d67c2f9c77671f2f5aa
A2L5E0X1 <alexed25@protonmail.com>
2022-09-23 23:14:03 +0200

    m12: add ota assert for f12
    * libinit sets the device prop in recovery so the flash would fail on F12 devices without assert
    
    Change-Id: I2ce263834b6fd809dfd87b23cd69d27b3e7a74fd

--- device.mk
+++ device.mk
@@ -17,5 +17,8 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/keylayout/uinput-sec-fp.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-sec-fp.kl
 
+# OTA
+TARGET_OTA_ASSERT_DEVICE := f12,m12
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(DEVICE_PATH)/overlay
